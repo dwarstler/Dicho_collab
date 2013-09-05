d12cb888071c7dabc88184db2cc69798c1c68d97
dwarstler <dougie.warstler21@gmail.com>
2013-09-05 15:04:23 -0400

    adf

---
+++ gitTest/realBranch.h
@@ -0,0 +1,13 @@
+//
+//  realBranch.h
+//  gitTest
+//
+//  Created by Doug Warstler on 9/5/13.
+//  Copyright (c) 2013 Doug Warstler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface realBranch : NSObject
+
+@end
---
+++ gitTest/realBranch.m
@@ -0,0 +1,13 @@
+//
+//  realBranch.m
+//  gitTest
+//
+//  Created by Doug Warstler on 9/5/13.
+//  Copyright (c) 2013 Doug Warstler. All rights reserved.
+//
+
+#import "realBranch.h"
+
+@implementation realBranch
+
+@end
